1b63be4cc11871e1625880d04eb5c486e14f07ae
Ahmad Ramdan <ahmd.rmdn@gmail.com>
2020-04-16 20:56:07 +0300

    display thermostat reading history

--- src/app/thermostat-details/thermostat-details.component.ts
+++ src/app/thermostat-details/thermostat-details.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from "@angular/core";
 import { ActivatedRoute } from "@angular/router";
 import { ThermostatService } from "../shared/services/thermostat.service";
 import { Thermostat } from "../shared/models/thermostat.model";
+import { PageEvent } from "@angular/material/paginator";
+import { ThermostatReading } from "../shared/models/thermostat-reading.model";
+import { ThermostatReadingService } from "../shared/services/thermostat-reading.service";
 
 @Component({
   selector: "app-thermostat-details",
@@ -9,19 +12,51 @@ import { Thermostat } from "../shared/models/thermostat.model";
   styleUrls: ["./thermostat-details.component.scss"],
 })
 export class ThermostatDetailsComponent implements OnInit {
+  length = 100;
+  pageSize = 10;
+  pageSizeOptions: number[] = [5, 10, 25, 100];
+
+  // MatPaginator Output
+  pageEvent: PageEvent;
+  displayedColumns: string[] = [
+    "ID",
+    "temperature",
+    "humidity",
+    "battery_charge",
+  ];
+
   thermostat_id: any;
   thermostat: Thermostat;
+  thermostat_readings: ThermostatReading[];
 
-  constructor(private route: ActivatedRoute, private api: ThermostatService) {}
+  constructor(
+    private route: ActivatedRoute,
+    private thermostatService: ThermostatService,
+    private readingService: ThermostatReadingService
+  ) {}
 
   ngOnInit(): void {
     this.thermostat_id = this.route.snapshot.paramMap.get("thermostat_id");
-    this.api.getThermostatById(this.thermostat_id).subscribe((data) => {
-      if (data.body) {
-        this.thermostat = data.body;
-      } else {
-        console.log("errorssss");
-      }
-    });
+    this.thermostatService
+      .getThermostatById(this.thermostat_id)
+      .subscribe((data) => {
+        if (data.body) {
+          this.thermostat = data.body;
+        } else {
+          console.log("errorssss");
+        }
+      });
+    this.readingService
+      .getThermostatReadings(this.thermostat_id)
+      .subscribe((data) => {
+        if (data.body) {
+          this.thermostat_readings = data.body;
+        } else {
+          console.log("errorssss");
+        }
+      });
+  }
+  changePage(event?: PageEvent) {
+    return event;
   }
 }
